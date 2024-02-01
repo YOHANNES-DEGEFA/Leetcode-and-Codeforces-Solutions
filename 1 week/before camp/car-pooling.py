91c9cab6f9207a3f5117aa2fe07a5d38233cab45
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2024-02-01 14:53:27 +0300

    Added solution for Car Pooling

--- 1 week/before camp/car-pooling.py
+++ 1 week/before camp/car-pooling.py
@@ -2,15 +2,14 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
 
 
-        furthest_dist = 0    
-        # find the furthest destination place
+        n = 0      # n = the furthest destination place 
+        
         for trip in trips: 
-            furthest_dist = max(furthest_dist, trip[2])
-        # use the furthest destination + 1 as your cumulative sum array length
-
-        arr_len = furthest_dist + 1
+            n = max(n, trip[2])
+    
+        arr_len = n + 1
         cum_sum = [0]*(arr_len)
-        print(arr_len)
+      
         for numPass, start, end in trips: 
             cum_sum[start] += numPass
             cum_sum[end]   -= numPass
