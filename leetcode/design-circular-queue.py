d12d8863deae201315b4e0c5e95c929f1aa4a384
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2024-02-22 22:59:38 +0300

    Add solution for Design Circular Queue

---
+++ leetcode/design-circular-queue.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, val: int):
+        self.val = val
+        self.next = None
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        # dummy node for head
+        self.head = Node(0)
+        self.rear = self.head
+        self.k = k
+        self.size = 0
+
+    def enQueue(self, value: int) -> bool:
+        # check size
+        if self.size == self.k: # full now 
+            return False
+        else:
+            node = Node(value)
+            # add to rear
+            self.rear.next = node
+            self.rear = node
+            self.size += 1
+            return True
+        
+
+    def deQueue(self) -> bool:
+        if self.size == 0:
+            return False
+        else: # has some element
+            # one element case
+            if self.head.next.next is None:
+                self.rear = self.head
+
+            self.head.next = self.head.next.next
+            self.size -= 1
+            return True
+        
+
+    def Front(self) -> int:
+        if self.size == 0:
+            return -1
+        else:
+            return self.head.next.val
+        
+
+    def Rear(self) -> int:
+        if self.size == 0:
+            return -1
+        else:
+            return self.rear.val
+
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
+        
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
+
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
