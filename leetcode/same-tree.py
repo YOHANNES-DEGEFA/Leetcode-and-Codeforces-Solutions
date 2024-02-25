33182dd5ec69076cbfb7b5f024bb5378b5106f19
YOHANNES-DEGEFA <138358695+YOHANNES-DEGEFA@users.noreply.github.com>
2024-02-26 01:11:32 +0300

    Add solution for Same Tree

--- leetcode/same-tree.py
+++ leetcode/same-tree.py
@@ -5,14 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, r1: Optional[TreeNode], r2: Optional[TreeNode]) -> bool:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 
-     
-        if not r1 and not r2: 
-            return True 
-        if (r1 and not r2) or (not r1 and r2) or (r1.val != r2.val):
-            return False 
-        return self.isSameTree(r1.left,r2.left) and self.isSameTree(r1.right,r2.right)
+        def checker(r1, r2): 
+
+            if not r1 and not r2: return True 
+            if not r1 and r2: return False 
+            if not r2 and r1: return False 
+
+            if r1.val != r2.val: return False 
+
+            return checker(r1.left,r2.left) and checker(r1.right,r2.right)
+
+        return checker(p,q)
 
 
+        
+
+
+        
         
